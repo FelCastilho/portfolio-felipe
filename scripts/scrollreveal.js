6e26fb498650d222b63d03337d6834339e632f97
felipeCast <castfelipe10@gmail.com>
2023-10-27 07:59:00 -0300

    Adicionando scrollreveal e video na aba equilibrium

---
+++ scripts/scrollreveal.js
@@ -0,0 +1,64 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('#behance-icon', {
+    duration: 2000,
+    rotate: {x: 0, y: 10, z: 0}
+})
+
+sr.reveal('#linkedin-icon', {
+    duration: 2500,
+    rotate: {x: 0, y: 10, z: 0}
+})
+
+sr.reveal('#instagram-icon', {
+    duration: 3000,
+    rotate: {x: 0, y: 10, z: 0}
+})
+
+sr.reveal('#github-icon', {
+    duration: 3500,
+    rotate: {x: 0, y: 10, z: 0}
+})
+
+
+sr.reveal('#html-icon', {
+    duration: 2000,
+    distance: '-20%'
+})
+
+sr.reveal('#css-icon', {
+    duration: 3000,
+    distance: '-20%'
+})
+
+sr.reveal('#js-icon', {
+    duration: 4000,
+    distance: '-20%'
+})
+
+sr.reveal('#sass-icon', {
+    duration: 5000,
+    distance: '-20%'
+})
+
+sr.reveal('#react-icon', {
+    duration: 6000,
+    distance: '-20%'
+})
+
+sr.reveal('#git-icon', {
+    duration: 7000,
+    distance: '-20%'
+})
+
+sr.reveal('#figma-icon', {
+    duration: 8000,
+    distance: '-20%'
+})
+
+sr.reveal('#photoshop-icon', {
+    duration: 9000,
+    distance: '-20%'
+})
+
+
