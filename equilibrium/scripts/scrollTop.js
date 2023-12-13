5b29f6ef48ab1a5d4952f50c5646df9ca946aecf
felipeCast <castfelipe10@gmail.com>
2023-12-13 14:45:46 -0300

    adicionando botao de retornar para o inicio

--- equilibrium/scripts/scrollTop.js
+++ equilibrium/scripts/scrollTop.js
@@ -1,14 +1,17 @@
-function scrollTop(){
-
-    let fabButton = document.getElementById('fab-button');
-    let homeScreen = document.getElementById('home-screen');
-    let apresentationScreen = document.getElementById('apresentation-screen');
-
-    if(window.screenY == apresentationScreen){
-        fabButton.style.visibility = 'visible';
-    }
+let fabButton = document.getElementById('fab-button');
+let apresentationScreen = document.getElementById('apresentation-screen');
+let objectiveScreen = document.getElementById('objective-screen');
 
 
+function scrollTop() {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= apresentationScreen.offsetTop) {
+            fabButton.style.visibility = 'visible';
+
+        }else{
+            fabButton.style.visibility = 'hidden';
+        }
+    });
 }
 
 scrollTop();
